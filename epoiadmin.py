70fffb17dc52aa1b71e441952697f9548b26769a
Stefan W <sw1@monton.de>
2011-08-30 22:43:27 -0400

    osm tag administration basically works

--- epoiadmin.py
+++ epoiadmin.py
@@ -3,6 +3,7 @@ import os
 from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext import webapp
+from google.appengine.ext.db import Key
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 from epoidbm import Epoicon, Osmtag
@@ -44,7 +45,7 @@ class EpoiAdminPage(webapp.RequestHandler):
         logging.debug ("%3d     iconfiles found: %s" % (len(iconfiles),[", ".join(iconfiles)]))
 
         # find all existing epoi icons in the storage
-        dbicons = Epoicon.all().fetch(1000)
+        dbicons = Epoicon.all()
         dbiconfiles = [dbi.file for dbi in dbicons]
         logging.debug ("%3d icons in storage" % (len(dbiconfiles)))
 
@@ -71,18 +72,141 @@ class EpoiAdminPage(webapp.RequestHandler):
             dbi.put()
         logging.debug ("%3d iconfiles imported:  %s" % (len(imported_icons),[",".join(imported_icons)]))
 
-        # read again after import
-        dbicons = Epoicon.all()
-        logging.debug ("%3d icons in database" % (dbicons.count()))
+        #
+        # Define data structure for the template
+        # categories = List of category/[epicon] tuples for the icons
+        # categories(1) ---- category string
+        # categories(2) ---- list of epoicons
+        #                      +-- name = clear text name (e.g. Hotel)
+        #                      +-- icon = png icon image
+        #                      +-+ osm_tags = a list of osm_tag key-value pairs
+        #                        +-- k
+        #                        +-- v
+        #
+        categories = []
+        # categorize icons based on their osm_tags
+        # (an icon can be in more than one category)
+        osmtags = Osmtag.all()
+        for tag in osmtags:
+            icon_with_tag = Epoicon.all()
+            icon_with_tag.filter("osm_tags =", tag.key())
+            icons = []
+            for epi in icon_with_tag:
+                icons.append({'file': os.path.join('icon',epi.file),
+                             'id':str(epi.key()),
+                             'name':epi.name})
+            categories.append((tag.k, icons))
 
-        template_values['icons'] = [dbi.file for dbi in dbicons]
+        # find unused icons in the data storage
+        unused = []
+        for epi in Epoicon.all():
+            if len(epi.osm_tags) < 1:
+                unused.append({'file': os.path.join('icon',epi.file),
+                               'id':str(epi.key()),
+                               'name':epi.name})
+        categories.append(('not used', unused))
+
+        template_values['categories'] = categories
 
         # render administration page
         path = os.path.join(os.path.dirname(__file__), 'epoiadmin.html')
         self.response.out.write(template.render(path, template_values))
 
+class EpoiAdminEditIconPage(webapp.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
 
-application = webapp.WSGIApplication([('/epoiadmin', EpoiAdminPage),
+        # TODO: Provide a decorator function for these checks
+        # not logged in
+        if not user:
+            self.redirect(users.create_login_url(self.request.uri))
+            return
+
+        template_values = {'nickname': user.nickname()}
+
+        # not an administrator
+        if not users.is_current_user_admin():
+            path = os.path.join(os.path.dirname(__file__), 'sorrynoadmin.html')
+            self.response.out.write(template.render(path, template_values))
+            return
+
+        # retrieve icon key and check it
+        id = self.request.get('id')
+        if not id:
+            logging.error ('id parameter missing in URL')
+            self.error(500)
+        key = self.request.get('key')
+        value = self.request.get('value')
+        action = self.request.get('action')
+
+        logging.debug ('id: %s (key: %s value: %s) action: %s' % (id,key,value,action))
+        epoicon = Epoicon.get(id)
+        if not epoicon:
+            self.error(500)
+
+        # store key/value pair if add was pressed
+        if action == 'add' and key and value:
+            # don't save duplicates in Osmtag entity
+            qosmtag = Osmtag.all()
+            qosmtag.filter('k =', key)
+            qosmtag.filter('v =', value)
+            osmtag = qosmtag.fetch(1)
+            if len(osmtag) < 1:
+                osmtag = Osmtag(k=key, v=value)
+                osmtag.put()
+            else:
+                osmtag = osmtag[0]
+            # store relation but don't save duplicates in list either
+            if not osmtag.key() in epoicon.osm_tags:
+                epoicon.osm_tags.append(osmtag.key())
+                epoicon.put()
+        elif action == 'delete':
+            # delete icon definition
+            epoicon.delete()
+            # go back to icon list page
+            self.redirect('/epoiadmin')
+        elif action.startswith('tagdelete'):
+            # get osmtag key from action string
+            key = action.split("_")[-1]
+            logging.debug("action tagdelete key: %s" % (key))
+            epoicon.osm_tags.remove(Key(key))
+        elif action == 'clone':
+            # clone icon definition
+            epoicon1 = Epoicon(name=epoicon.name,
+                              file=epoicon.file,
+                              icon=epoicon.icon,
+                              osm_tags=epoicon.osm_tags)
+            epoicon1.put()
+        elif action == 'done':
+            # go back to icon list page
+            self.redirect('/epoiadmin')
+        else:
+            pass
+
+
+        # retrieve osm tags
+        osmtags = []
+        for osmtag_key in epoicon.osm_tags:
+            osmtag = Osmtag.get(osmtag_key)
+            osmtags.append({'id': str(osmtag_key),
+                            'k': osmtag.k,
+                            'v': osmtag.v})
+
+        icon = {'file': os.path.join('icon',epoicon.file),
+                'id': str(epoicon.key()),
+                'name':epoicon.name}
+
+        template_values['icon'] = icon
+        template_values['osmtags'] = osmtags
+
+        # render administration page
+        path = os.path.join(os.path.dirname(__file__), 'epoiadminicon.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+
+application = webapp.WSGIApplication([('/epoiadmin/icon.*', EpoiAdminEditIconPage),
+                                      ('/epoiadmin', EpoiAdminPage),
                                       ('/login', LoginPage),
                                       ('/logout', LogoutPage)],debug=True)
 
--- epoidbm.py
+++ epoidbm.py
@@ -1,3 +1,4 @@
+import geo.geomodel
 from google.appengine.ext import db
 
 class Osmtag(db.Model):
@@ -10,29 +11,35 @@ class Epoicon(db.Model):
     An epoi tag represents a single osm_tag (such as tourism: hotel)
     or a combination of tags (tourism: hotel; stars: 3)"""
     name = db.StringProperty(required=True)
-    # file name of icon
+    # file name of icon (includes path)
     file = db.StringProperty(required=True)
     # the icon itself
     icon = db.BlobProperty(required=True)
     # references to one or more osm_tags
     osm_tags = db.ListProperty(db.Key)
 
-
-class Epoi(db.Expando):
-    """Point of interest as was loaded from the OSM database at timestamp"""
+class Epoi(geo.geomodel.GeoModel):
+    """Point of interest as was loaded from the OSM database"""
     # 64 bit osm key
     osm_id = db.IntegerProperty(required=True)
-    # lat/lon location
-    location = db.GeoPtProperty(required=True)
-    # date/time when loaded from the OSM database
-    timestamp = db.DateTimeProperty(required=True)
-    # English name which appears (more name properties may be added
-    # such as name_de, name_es)
+    # lat/lon location is inherited from GeoModel
+    # node's version in the osm database
+    version = db.IntegerProperty(required=True)
+    # English name which is used for display.
+    # name properties in other languages may be added
+    # in Epoi_ext entity ()such as name_de, name_es)
     name = db.StringProperty(required=True)
     # web address of the place
     web = db.LinkProperty()
+    # a list of icons (categories) which apply to the object.
+    # Why more than one? The node may represent a hotel AND a restaurant
+    epoicons = db.ListProperty(db.Key)
+
+class Epoi_ext(db.Expando):
+    """Extended properties for an epoi entity. It is defined
+    as a separate entity because I need to use GeoModel as a
+    base class for Epoi. GeoModel is based on db.Model .
+    When using extended properties (like name_es) the epoi
+    instance must be defined as the epoi_ext parent."""
     # a list of pics of the place
     pics = db.ListProperty(db.Blob)
-    # a list of tags (categories) which apply to the object.
-    # Why more than one? The node may represent a hotel AND a restaurant
-    categs = db.ListProperty(db.Key)
